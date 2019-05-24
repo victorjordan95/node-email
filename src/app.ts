0fd3d5a12c494664082e59011d2c2410bd42f05e
Victor Jordan <victor.jordan@gauge.local>
2019-05-24 16:38:10 -0300

    adding email api

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import routes from './routes'
+
+class App {
+  public express: express
+
+  public constructor () {
+    this.express = express()
+
+    this.middlawares()
+    this.routes()
+  }
+
+  private middlawares ():void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  private routes (): void {
+    this.express.use(routes)
+  }
+}
+
+export default new App().express
---
+++ src/routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express'
+import nodemailer from 'nodemailer'
+
+const routes = Router()
+
+routes.post('/send', function (req, res, next): void {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'victor.jordan@gauge.com.br',
+      pass: 'viku1995'
+    }
+  })
+  const mailOptions = {
+    from: `${req.body.email}`,
+    to: `${req.body.recipient}`,
+    subject: `${req.body.subject}`,
+    text: `${req.body.text}`,
+    replyTo: `victorjordan95@gmail.com`
+  }
+  transporter.sendMail(mailOptions, function (err, res): any {
+    if (err) {
+      console.error('there was an error: ', err)
+      return res.status(400).send(err)
+    }
+    console.log(res)
+  })
+  return res.status(200).send(res)
+})
+
+export default routes

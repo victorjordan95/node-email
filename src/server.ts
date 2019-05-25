323fa097e92f3242ce4da0a1810c15dc23ab7e43
Victor Jordan <victorjordan95@gmail.com>
2019-05-24 23:09:36 -0300

    fix server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
 import app from './app'
 
-app.listen(3333)
+// app.listen(3333)
+const port = process.env.PORT || 8080
+
+app.listen(port, function (): void {
+  console.log('Our app is running on http://localhost:' + port)
+})
